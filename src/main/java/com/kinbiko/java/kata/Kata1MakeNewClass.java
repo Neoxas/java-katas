11b5805ea64f485a7d6fe3ef76668022b7970940
Roger Guldbrandsen <roger@kinbiko.com>
2016-05-13 19:00:00 +0100

    Added first kata

---
+++ src/main/java/com/kinbiko/java/kata/Kata1MakeNewClass.java
@@ -0,0 +1,12 @@
+package com.kinbiko.java.kata;
+
+/**
+ * Create and return an instance of a new class, created in the same package as this class.
+ */
+public class Kata1MakeNewClass {
+	
+	public Object testMe(){
+		return null;
+	}
+
+}
