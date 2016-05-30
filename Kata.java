8a015f1938c5c9e03f20680c70238ee7759fef83
Roger Guldbrandsen <roger@kinbiko.com>
2016-05-30 23:29:06 +0100

    Removed leftover file

--- Kata.java
+++
@@ -1,9 +0,0 @@
-public class Kata {
-    static String name = "Gojushiho Sho";
-    static Integer numberOfMoves = 54;
-
-    public static void main(String[] args){
-        System.out.println(name);
-        System.out.println(numberOfMoves);
-    }
-}
