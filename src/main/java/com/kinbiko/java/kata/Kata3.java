c1bc1fb09eea6e95f1b4e22a4258b7dc418827d7
Roger Guldbrandsen <roger@kinbiko.com>
2016-05-31 23:08:56 +0100

    Removed katas.

--- src/main/java/com/kinbiko/java/kata/Kata3.java
+++
@@ -1,9 +0,0 @@
-package com.kinbiko.java.kata;
-
-/*
- * Write a class implementing an interface InvocationCounter, with a method which returns 
- * twice the number of times this method has been called.
- */
-public class Kata3 {
-    
-}
